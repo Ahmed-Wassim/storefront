b9f6bdd2415703e2a2064d75a4912895c6e7333d
Ahmed Wassim <ahmedwassim317@gmail.com>
2025-09-23 14:04:18 +0300

    refactor serialization for store app

--- store/views.py
+++ store/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -9,7 +8,7 @@ from .serializers import ProductSerializer
 # Create your views here.
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
+    queryset = Product.objects.select_related("collection").all()
     serializer = ProductSerializer(queryset, many=True)
     return Response(serializer.data)
 
